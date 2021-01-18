d374118d474efa55f16d71f56a3d29eb6e2ac187
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-18 15:52:26 -0300

    Developed newRoom, save, onSubmit and goToList on create-room.component

--- src/app/create-room/create-room.component.ts
+++ src/app/create-room/create-room.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-create-room',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRoomComponent implements OnInit {
 
-  constructor() { }
+  room!: Room
+  submitted!: boolean
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newRoom() {
+    this.submitted = false
+    this.room = new Room()
+  }
+
+  save() {
+    this.roomService.createRoom(this.room)
+      .subscribe(data => console.log(data),
+      error => console.log(error))
+
+    this.goToList()
+  }
+
+  onSubmit() {
+    this.submitted = true
+    this.save()
+  }
+
+  goToList() {
+    this.router.navigate(['/rooms'])
   }
 
 }
