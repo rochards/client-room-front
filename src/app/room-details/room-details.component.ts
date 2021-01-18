a7474d1fc1186ac5c6527977069018d440284e11
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-18 16:51:49 -0300

    Changed ngOnInit method. Added list method.

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-details',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomDetailsComponent implements OnInit {
 
-  constructor() { }
+  id!: number
+  room!: Room
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomService, private router: Router, 
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.room = new Room()
+    this.id = this.route.snapshot.params['id']
+    this.roomService.getRoom(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.room = data;
+      }, error => console.log(error))
+  }
+
+  list() {
+    this.router.navigate(['/rooms'])
   }
 
 }
